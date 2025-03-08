c9da041e7091b189f501a9b2c47b64e44701ff32
typename-russianbot <axix01@yahoo.com>
2025-03-08 01:09:33 -0800

    updating...

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Program | Password.Keychain | by Matthew H.
 
 |--- Program Function ---| 
+
+- Classes Defined in "master.h":
+    1. Keychain
+        - keynode: represents the 'links in the chain' of our digital keychain
+        - key: represents an individual profile passkey
+    2. Keychain_Account
+        - tbd
+        
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
 #include "master.h"
-
 //& |--- MAIN ---|
-int main(void)
-{
-    if (debugger == _off)
-    {
-        system("clear");
-    }
+int main(void) {
 
-    cout << "||==== Keychain ====||==== {v.0.0.1} ====||" << endl; //* v0.0.1
-    //* test keys
-    key key1, key2;                                                
+  cout << "||==== Keychain ====||==== {v.0.0.1} ====||" << endl; //* v0.0.1
+  //* test keys
+  key key1, key2;
 
-    //* test nodes
-    keynode *keynode2 = new keynode(key2);
-    keynode *keynode1 = new keynode(key1);
+  //* test nodes
+  keynode *keynode2 = new keynode(key2);
+  keynode *keynode1 = new keynode(key1);
 
-    //* testing bay
-    keynode1->set_mNext(keynode2);
+  //* testing bay
+  keynode1->set_mNext(keynode2);
 
-    cout << "Keynode1 Data: " << endl;
-    cout << keynode1->get_mData() << endl;
-    cout << endl;
+  cout << "Keynode1 Data: " << endl;
+  cout << keynode1->get_mData() << endl;
+  cout << endl;
 
-    keynode1 = keynode1->get_mNext();
-    cout << "mNext() data: " << endl;
-    cout << keynode1->get_mData() << endl;
-    cout << "||=======================================||" << endl; //* end of keychain
-    return 0;
+  keynode1 = keynode1->get_mNext();
+  cout << "mNext() data: " << endl;
+  cout << keynode1->get_mData() << endl;
+  cout << "||=======================================||" //* END OF PROGRAM
+       << endl;                                         //* end of keychain
+  return 0;
 }
